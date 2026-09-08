8754432eed0316e69beaad2d434bf09a297e66a3
WarllesNoletoTech <warllesb@gmail.com>
2026-09-08 19:51:46 -0300

    feat: bootstrap multi-tenant digital menu platform

---
+++ backend/src/orders/orders.module.ts
@@ -0,0 +1,2 @@
+import { Module } from '@nestjs/common'; import { MongooseModule } from '@nestjs/mongoose'; import { Order, OrderSchema } from '../common/schemas'; import { OrdersController } from './orders.controller'; import { OrdersService } from './orders.service';
+@Module({ imports: [MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }])], controllers: [OrdersController], providers: [OrdersService] }) export class OrdersModule {}
